91c53ca62d33055b5913072bc8470bbcb74fce30
IsaacFayle-Waters <user_email>
2026-08-15 18:04:07 +0000

    Tampered with learning rate from 5e-5 to 3e-4

--- src/marl_tsc/mappo.py
+++ src/marl_tsc/mappo.py
@@ -317,7 +317,7 @@ def train_mappo(
     
     critic = Critic(obs_dim * num_agents).to(device)
 
-    learning_rate = 5e-5
+    learning_rate = 3e-4 #5e-5
     optimiser = Adam(
         list(actor.parameters()) + list(critic.parameters()),
         lr=learning_rate,
